acc67cb8f4f61082df53020a815b20aa153c536c
berTrapp <bernardo.trappp@gmail.com>
2024-12-04 19:43:07 -0300

    Calculadora Finalizada

--- src/test/java/com/bertrapp/PrimeiroTeste.java
+++ src/test/java/com/bertrapp/PrimeiroTeste.java
@@ -3,6 +3,8 @@ package com.bertrapp;
 import static org.assertj.core.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -10,10 +12,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 public class PrimeiroTeste {
 
+    Calculadora calculadora;
+
+    @BeforeEach
+    public void setUp(){
+        calculadora = new Calculadora();
+    }
+
     @Test
     public void deveSomar2Numeros(){
         //cenario
-        Calculadora calculadora = new Calculadora();
         int numero1 = 10, numero2 = 5;
 
         //execução
@@ -26,7 +34,6 @@ public class PrimeiroTeste {
     @Test
     public void naoDeveSomarNumerosNegativos(){
         //cenario
-        Calculadora calculadora = new Calculadora();
         int numero1 = 10, numero2 = -4;
 
         //execução
@@ -34,6 +41,52 @@ public class PrimeiroTeste {
                 .isInstanceOf(RuntimeException.class)
                 .hasMessage("Não é permitido somar números negativos");
 
+    }
+
+    @Test
+    public void deveSubtrair2Numeros(){
+        //cenario
+        int numero1 = 10, numero2 = 3;
+
+        //execução
+        int resultado = calculadora.subtrair(numero1, numero2);
+
+        //verificação
+        Assertions.assertThat(resultado).isEqualTo(7);
+    }
+
+    @Test
+    public void deveMultiplicar2Numeros(){
+        //cenario
+        int numero1 = 9, numero2 = 4;
+
+        //execução
+        int resultado = calculadora.multiplicar(numero1, numero2);
+
+        //verificação
+        Assertions.assertThat(resultado).isEqualTo(36);
+    }
+
+    @Test
+    public void deveDividir2Numeros(){
+        //cenario
+        int numero1 = 27, numero2 = 3;
+
+        //execução
+        float resultado = calculadora.dividir(numero1, numero2);
+
+        //verificação
+        Assertions.assertThat(resultado).isEqualTo(9);
+    }
+
+    @Test
+    public void naoDeveDividirPor0(){
+        //cenario
+        int numero1 = 17, numero2 = 0;
+
+        //execução
+        assertThatThrownBy(() -> calculadora.dividir(numero1, numero2))
+                .isInstanceOf(ArithmeticException.class);
     }
 }
 
@@ -44,5 +97,18 @@ class Calculadora {
         }else {
             return num + num2;
         }
+    }
+
+    int subtrair(int num, int num2){
+        return num - num2;
+    }
+
+    int multiplicar(int num, int num2){
+        return num * num2;
+    }
+
+    float dividir(int num, int num2){
+        return num / num2;
+
     }
 }
